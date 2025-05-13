74d79ee8d5b952fc7099b84291bb1dd8d106123e
Tom Dimiduk <tom@dimiduk.net>
2025-05-13 12:21:10 -0700

    start database setup

--- backend/src/Backend.hs
+++ backend/src/Backend.hs
@@ -1,10 +1,33 @@
 module Backend where
 
-import Common.Route
+import Control.Monad.IO.Class
+import System.Directory
+
+import Gargoyle.PostgreSQL.Connect
 import Obelisk.Backend
+
+import Common.Route
+
+import Backend.Database.Migrate
+
+data BackendEnv = BackendEnv
+  { _dbPath :: String }
+
+getBackendEnv :: IO BackendEnv
+getBackendEnv = do
+  dbConfigExists <- liftIO $ doesFileExist "config/backend/db"
+  let dbPath = if dbConfigExists then "config/backend/db" else "db"
+  pure $ BackendEnv
+    { _dbPath = dbPath }
 
 backend :: Backend BackendRoute FrontendRoute
 backend = Backend
-  { _backend_run = \serve -> serve $ const $ return ()
+  { _backend_run = \serve -> do
+      env <- getBackendEnv
+      withDb ( _dbPath env ) $ \dbConnPool -> do
+        migration dbConnPool
+        serve $ const $ pure ()
+
+
   , _backend_routeEncoder = fullRouteEncoder
   }
